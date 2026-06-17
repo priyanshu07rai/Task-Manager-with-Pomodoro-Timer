1af2c80b71fefbf21bdbec701e74866f468b16d5
Harshit Harsh <harshitharsh659@gmail.com>
2026-06-17 22:16:53 +0530

    Login_added

--- backend-drf/accounts/serializers.py
+++ backend-drf/accounts/serializers.py
@@ -9,12 +9,15 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
-    def NewAcc(self, validate_data):
+    def create(self, validated_data):
         user = User.objects.create_user(
-            username = validate_data['username'],
-            email = validate_data['email'],
-            password = validate_data['password'],
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
         )
     
         return user
