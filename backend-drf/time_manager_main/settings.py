42d09e0c1f0e31f9d009e4aec03d06948f0c5b04
Harshit Harsh <harshitharsh659@gmail.com>
2026-06-26 15:51:20 +0530

    Update settings.py

--- backend-drf/time_manager_main/settings.py
+++ backend-drf/time_manager_main/settings.py
@@ -100,7 +100,7 @@ REST_FRAMEWORK = {
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "/var/data/db.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
